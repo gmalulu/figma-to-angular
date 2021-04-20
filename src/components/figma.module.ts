7c1929b82ff7e29e7bf5e5781fd6db0661512cb9
gmalulu <gracemalulu@gmail.com>
2021-04-20 11:10:25 +0100

    updated
    updated

--- src/components/figma.module.ts
+++ src/components/figma.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { CClockComponent } from './CClock.component';
-import { CFClockComponent } from './CFClock.component';
 
 @NgModule({
   imports: [ CommonModule, FormsModule ],
-  declarations: [ CClockComponent, CFClockComponent ],
-  exports: [ CClockComponent, CFClockComponent ],
+  declarations: [  ],
+  exports: [  ],
 })
 export class FigmaModule { }
